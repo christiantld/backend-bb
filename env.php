aa52c4eefd64d5dd5560bf02575a25b1577e0b52
Christian Toledo <christian.odt@gmail.com>
2019-11-28 02:03:06 -0300

    ..

---
+++ env.php
@@ -0,0 +1,10 @@
+<?php
+putenv('DISPLAY_ERRORS_DETAILS=' . true);
+
+putenv('MYSQL_HOST=us-cdbr-iron-east-05.cleardb.net');
+putenv('MYSQL_DBNAME=heroku_575ef0244b858ab');
+putenv('MYSQL_USER=bd5b4332d5d91f');
+putenv('MYSQL_PASSWORD=7ff72c77');
+putenv('MYSQL_PORT=3306');
+
+putenv('JWT_SECRET_KEY=$2y$10$PdUwrzfpZS21FaXVyCvHhugeEOKCGp2v0jm7U24e2cRqmFLa2sITW');
